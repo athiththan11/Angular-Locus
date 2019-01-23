c447ee8e019e7b5adc7005368a62f69e70b61c64
Athiththan <code19111995@gmail.com>
2019-01-24 00:08:51 +0530

    bottom sheet data display

--- src/app/bottom-sheet/bottom-sheet.component.ts
+++ src/app/bottom-sheet/bottom-sheet.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+// Angular Material Bottom Sheet Data
+import { MAT_BOTTOM_SHEET_DATA } from '@angular/material';
+
+// Site model
+import { Site } from '../../models/site.model';
 
 @Component({
   selector: 'app-bottom-sheet',
@@ -6,10 +12,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bottom-sheet.component.css']
 })
 export class BottomSheetComponent implements OnInit {
+  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public site: Site) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
