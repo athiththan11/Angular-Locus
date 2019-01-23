4d7cb215febf1c0b138a061517a0dbea1c0702ed
Athiththan <code19111995@gmail.com>
2019-01-24 00:10:26 +0530

    env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  GOOGLE_MAPS_API_KEY: 'YOUR_API_KEY',
+  firebaseConfig: {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    databaseURL: 'YOUR_DATABASE_URL',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID'
+  }
 };
