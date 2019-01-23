b75ffaccd3582f3fad17452a4e1b01fd3b4de317
Athiththan <code19111995@gmail.com>
2019-01-23 12:35:35 +0530

    angular material module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Site model
+import { Site } from '../models/site.model';
+import { LocationService } from './location.service';
+
+// Angular Material Bottom Sheet component
+import { MatBottomSheet } from '@angular/material';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-locus';
+export class AppComponent implements OnInit {
+  title = 'Angular Locus';
+
+  lat: number;
+  lng: number;
+  zoom = 16;
+
+  constructor(private locationService: LocationService, private bottomSheet: MatBottomSheet) {}
+
+  ngOnInit(): void {
+    // sets the current position of user
+    this.setCurrentPosition();
+  }
+
+  /**
+   * sets current position of the user
+   * if location service is enabled and
+   * allowed to access user's geo location
+   */
+  private setCurrentPosition() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+      });
+    }
+  }
+
+  /**
+   * displays angular bottom sheet
+   */
+  public displayBottomSheet() {
+    this.bottomSheet.open(BottomSheetComponent, { data: 'pass data here' });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
+
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BottomSheetComponent],
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
+  entryComponents: [BottomSheetComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
